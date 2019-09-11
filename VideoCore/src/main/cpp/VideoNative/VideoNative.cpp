280743044b7af270135c7329e9147993f3efaf44
di57mec <constantin.geier@lrz.de>
2019-09-11 17:43:07 +0200

    bind callback

--- VideoCore/src/main/cpp/VideoNative/VideoNative.cpp
+++ VideoCore/src/main/cpp/VideoNative/VideoNative.cpp
@@ -27,7 +27,7 @@ VideoNative::VideoNative(JNIEnv* env, jobject videoParamsChangedI,jobject contex
     //Store a global reference to it, so we can use it later
     //callToJava.globalJavaObj = env->NewGlobalRef(videoParamsChangedI); //also need a global javaObj
     callToJava.globalJavaObj = env->NewWeakGlobalRef(videoParamsChangedI);
-
+    
     test=new FFMpegVideoReceiver("",0,std::bind(&VideoNative::onNewNALU, this, std::placeholders::_1));
 }
 
