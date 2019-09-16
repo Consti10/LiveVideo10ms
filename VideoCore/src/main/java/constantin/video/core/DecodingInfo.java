475c33b7280e7eb0354f44112f400865c2ab9906
di57mec <constantin.geier@lrz.de>
2019-09-16 14:08:10 +0200

    update ffmpeg libraries

--- VideoCore/src/main/java/constantin/video/core/DecodingInfo.java
+++ VideoCore/src/main/java/constantin/video/core/DecodingInfo.java
@@ -11,19 +11,31 @@ public class DecodingInfo {
     public final float avgParsingTime_ms;
     public final float avgWaitForInputBTime_ms;
     public final float avgHWDecodingTime_ms; //time the hw decoder was holding on to frames. Not the full decoding time !
+    public final float avgDecodingTime_ms;
     public final int nNALU;
     public final int nNALUSFeeded;
 
+    public DecodingInfo(){
+        currentFPS=0;
+        currentKiloBitsPerSecond=0;
+        avgParsingTime_ms=0;
+        avgWaitForInputBTime_ms=0;
+        avgHWDecodingTime_ms=0;
+        nNALU=0;
+        nNALUSFeeded=0;
+        avgDecodingTime_ms=0;
+    }
 
-    public DecodingInfo(float currentFPS, float currentKiloBitsPerSecond,float avgParsingTime_ms,float avgWaitForInputBTime_ms,float avgDecodingTime_ms,
+    public DecodingInfo(float currentFPS, float currentKiloBitsPerSecond,float avgParsingTime_ms,float avgWaitForInputBTime_ms,float avgHWDecodingTime_ms,
                         int nNALU,int nNALUSFeeded){
         this.currentFPS=currentFPS;
         this.currentKiloBitsPerSecond=currentKiloBitsPerSecond;
         this.avgParsingTime_ms=avgParsingTime_ms;
         this.avgWaitForInputBTime_ms=avgWaitForInputBTime_ms;
-        this.avgHWDecodingTime_ms =avgDecodingTime_ms;
+        this.avgHWDecodingTime_ms =avgHWDecodingTime_ms;
         this.nNALU=nNALU;
         this.nNALUSFeeded=nNALUSFeeded;
+        this.avgDecodingTime_ms=avgParsingTime_ms+avgWaitForInputBTime_ms+avgHWDecodingTime_ms;
     }
 
     public Map<String,Object> toMap(){
@@ -36,6 +48,7 @@ public class DecodingInfo {
         decodingInfo.put("avgTotalDecodingTime_ms",(avgParsingTime_ms+avgWaitForInputBTime_ms+ avgHWDecodingTime_ms));
         decodingInfo.put("nNALU",nNALU);
         decodingInfo.put("nNALUSFeeded",nNALUSFeeded);
+        decodingInfo.put("avgDecodingTime_ms",avgDecodingTime_ms);
         return decodingInfo;
     }
 
@@ -48,7 +61,7 @@ public class DecodingInfo {
                 " avgParsingTime_ms:"+avgParsingTime_ms+
                 " avgWaitForInputBTime_ms:"+avgWaitForInputBTime_ms+
                 " avgHWDecodingTime_ms:"+ avgHWDecodingTime_ms +
-                " avgTotalDecodingTime_ms"+(avgParsingTime_ms+avgWaitForInputBTime_ms+ avgHWDecodingTime_ms)+
+                " avgDecodingTime_ms"+avgDecodingTime_ms+
                 " nNALU:"+nNALU+
                 " nNALUSFeeded:"+nNALUSFeeded;
     }
