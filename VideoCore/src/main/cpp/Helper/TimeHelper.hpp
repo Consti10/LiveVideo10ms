9868a36ed909fc05b460a621ec4c1cebd5069e25
Consti10 <geierconstantinabc@gmail.com>
2020-01-24 14:11:55 +0100

    migrated from LowLagDecoder

---
+++ VideoCore/src/main/cpp/Helper/TimeHelper.hpp
@@ -0,0 +1,49 @@
+//
+// Created by geier on 18/01/2020.
+//
+
+#ifndef LIVEVIDEO10MS_TIMEHELPER_HPP
+#define LIVEVIDEO10MS_TIMEHELPER_HPP
+
+class AvgCalculator{
+private:
+    long sum=0;
+    long sumCount=0;
+public:
+    AvgCalculator() = default;
+
+    void add(long x){
+        sum+=x;
+        sumCount++;
+    }
+    long getAvg(){
+        if(sumCount==0)return 0;
+        return sum/sumCount;
+    }
+    void reset(){
+        sum=0;
+        sumCount=0;
+    }
+};
+
+class RelativeCalculator{
+private:
+    long sum=0;
+    long sumAtLastCall=0;
+public:
+    RelativeCalculator() = default;
+    void add(long x){
+        sum+=x;
+    }
+    long getDeltaSinceLastCall() {
+        long ret = sum - sumAtLastCall;
+        sumAtLastCall = sum;
+        return ret;
+    }
+    long getAbsolute(){
+        return sum;
+    }
+};
+
+
+#endif //LIVEVIDEO10MS_TIMEHELPER_HPP
