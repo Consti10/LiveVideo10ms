7c80823cfcd4de3d92c9ffde5896025fda5a53fc
Constantin Geier <constantin.geier@lrz.de>
2020-07-05 13:55:37 +0200

    prefab enabled, dependency enabled - does not compile 2

--- uvcintegration/src/main/java/constantin/video/core/IVideoParamsChanged.java
+++
@@ -1,9 +0,0 @@
-package constantin.video.core;
-
-
-//Similar to INativeVideoParamsChanged,but instead of passing all float's it passes
-//a DecodingInfo instance. Not called by native code
-public interface IVideoParamsChanged{
-    void onVideoRatioChanged(int videoW, int videoH);
-    void onDecodingInfoChanged(final DecodingInfo decodingInfo);
-}
