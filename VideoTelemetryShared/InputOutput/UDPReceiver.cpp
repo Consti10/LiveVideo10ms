278503526e35e0e52303b8f85acc243c300e625c
constantin <geierconstantinabc@gamil.com>
2020-09-07 20:59:50 +0200

    uncommented the old "" style but left it there

--- VideoTelemetryShared/InputOutput/UDPReceiver.cpp
+++ VideoTelemetryShared/InputOutput/UDPReceiver.cpp
@@ -1,5 +1,7 @@
 
 #include "UDPReceiver.h"
+//#include "../Helper/AndroidThreadPrioValues.hpp"
+//#include "../Helper/NDKThreadHelper.hpp"
 #include <AndroidThreadPrioValues.hpp>
 #include <NDKThreadHelper.hpp>
 #include <arpa/inet.h>
