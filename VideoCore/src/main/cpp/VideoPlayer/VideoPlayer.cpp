c5312b523282cf48da971266aa06dd165f622f7c
ru26kif <constantin.geier@campus.lmu.de>
2020-10-09 13:06:40 +0200

    improve rtsp debugging

--- VideoCore/src/main/cpp/VideoPlayer/VideoPlayer.cpp
+++ VideoCore/src/main/cpp/VideoPlayer/VideoPlayer.cpp
@@ -121,12 +121,12 @@ void VideoPlayer::start(JNIEnv *env,jobject androidContext) {
         break;
         case VIA_FFMPEG_URL:{
             MLOGD<<"Started with SOURCE=RTSP(FFMPEG)";
-            //const std::string url=mSettingsN.getString(IDV::VS_FFMPEG_URL);
+            const std::string url=mSettingsN.getString(IDV::VS_FFMPEG_URL);
             //const std::string url="file:/storage/emulated/0/DCIM/FPV_VR/capture1.h264";
             //const std::string url="file:/storage/emulated/0/DCIM/FPV_VR/360_test.h264";
             //const std::string url="file:/storage/emulated/0/DCIM/FPV_VR/360.h264";
             //const std::string url="file:/storage/emulated/0/DCIM/FPV_VR/test.mp4";
-            const std::string url="https://www.radiantmediaplayer.com/media/big-buck-bunny-360p.mp4";
+            //const std::string url="https://www.radiantmediaplayer.com/media/big-buck-bunny-360p.mp4";
             //MLOGD("url:%s",url.c_str());
             mFFMpegVideoReceiver=std::make_unique<FFMpegVideoReceiver>(url,0,[this](uint8_t* data,int data_length) {
                 onNewVideoData(data,(size_t)data_length,VIDEO_DATA_TYPE::RAW);
