9bc2a79c5be972e1c4c44a178747c7bf22cd29b9
ru26kif <constantin.geier@campus.lmu.de>
2020-10-11 17:19:53 +0200

    refactoring ids

--- VideoCore/src/main/cpp/IDV.hpp
+++ VideoCore/src/main/cpp/IDV.hpp
@@ -5,7 +5,7 @@ class IDV{
 public:
     static constexpr const char* VS_PROTOCOL="Video stream protocol";
     static constexpr const char* VS_PORT="Video stream port";
-    static constexpr const char* VS_GROUND_RECORDING="VS GroundRecording";
+    static constexpr const char* VS_GROUND_RECORDING="VS_GROUND_RECORDING";
     static constexpr const char* VS_SOURCE="VS_SOURCE";
     static constexpr const char* VS_PLAYBACK_FILENAME="VS_PLAYBACK_FILENAME";
     static constexpr const char* VS_ASSETS_FILENAME_TEST_ONLY="VS_ASSETS_FILENAME_TEST_ONLY";
