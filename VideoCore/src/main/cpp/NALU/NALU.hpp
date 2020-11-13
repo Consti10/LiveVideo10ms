cfd3107b800582b03d54725b96eee6f164c4e617
ru26kif <constantin.geier@campus.lmu.de>
2020-11-13 03:56:50 +0100

    testing

--- VideoCore/src/main/cpp/NALU/NALU.hpp
+++ VideoCore/src/main/cpp/NALU/NALU.hpp
@@ -163,7 +163,7 @@ public:
                 MLOGD<<"Has vui"<<sps.parsed.vui_parameters_present_flag;
                 MLOGD<<"SPS Latency:"<<H264Stream::latencyAffectingValues(&sps.parsed);
             }else if(get_nal_unit_type()==NAL_UNIT_TYPE_PPS){
-                
+
             }
         }
     }
