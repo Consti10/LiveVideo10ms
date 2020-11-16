bc19773e8093ae34dd5b596846febeb9f56a0389
consti10 <geierconstantinabc@gmail.com>
2020-11-16 13:01:28 +0100

    testing

--- VideoCore/libs/h265nal/src/absl/types/optional.h
+++ VideoCore/libs/h265nal/src/absl/types/optional.h
@@ -3,6 +3,9 @@
 
 #include <optional>
 
+// the original h265nal library uses absl::optional since it cannot use c++17 where std::optional was introduced
+// Since c++17 is available on android ndk, I bring std::optional into the absl::optional namespace
+// such that the library compiles without absl
 namespace absl{
     template<class T>
     using optional=std::optional<T>;
