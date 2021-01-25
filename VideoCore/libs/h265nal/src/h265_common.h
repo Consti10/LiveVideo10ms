b479908ee24b78c4f95517256371de4a58dff88b
ru26kif <constantin.geier@campus.lmu.de>
2021-01-25 17:07:38 +0100

    testing

--- VideoCore/libs/h265nal/src/h265_common.h
+++ VideoCore/libs/h265nal/src/h265_common.h
@@ -119,6 +119,8 @@ static inline void XPrintf(FILE* fp,const char* fmt,...){
   va_end(args);
 }
 
+using MY_DUMP_IMPL=FILE*;
+
 void fdump_indent_level(FILE* outfp, int indent_level);
 void fdump_indent_level(std::stringstream& ss, int indent_level);
 }  // namespace h265nal
