1b2188b82bfe0b132daae06b96547dacb1d9476e
ru26kif <constantin.geier@campus.lmu.de>
2021-01-25 14:55:37 +0100

    work on common

--- VideoCore/libs/h265nal/src/h265_common.h
+++ VideoCore/libs/h265nal/src/h265_common.h
@@ -87,6 +87,30 @@ bool rbsp_trailing_bits(rtc::BitBuffer *bit_buffer);
 // fdump() indentation help
 int indent_level_incr(int indent_level);
 int indent_level_decr(int indent_level);
+
+#include <string>
+#include <sstream>
+static inline std::string string_format(const std::string fmt, ...) {
+  int size = ((int)fmt.size()) * 2 + 50;   // Use a rubric appropriate for your code
+  std::string str;
+  va_list ap;
+  while (1) {     // Maximum two passes on a POSIX system...
+    str.resize(size);
+    va_start(ap, fmt);
+    int n = vsnprintf((char *)str.data(), size, fmt.c_str(), ap);
+    va_end(ap);
+    if (n > -1 && n < size) {  // Everything worked
+      str.resize(n);
+      return str;
+    }
+    if (n > -1)  // Needed size returned
+      size = n + 1;   // For null char
+    else
+      size *= 2;      // Guess at a larger size (OS specific)
+  }
+  return str;
+}
+
 void fdump_indent_level(FILE* outfp, int indent_level);
 void sdump_indent_level(std::stringstream& ss, int indent_level);
 }  // namespace h265nal
